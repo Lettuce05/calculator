2f12c7d3e1081704511267d9d40c18590c5d2895
Anthony Romaine <anthony.romaine1@gmail.com>
2021-01-03 21:18:56 -0800

    added initial functionality

---
+++ app.js
@@ -0,0 +1,80 @@
+function add (left, right) {
+	return left + right;
+}
+
+function subtract (left, right) {
+	return left - right;
+}
+
+function multiply (left, right){
+    return left * right;
+}
+
+function divide (left, right){
+    return left / right;
+}
+
+function operate (op, left, right){
+    switch(op){
+        case "+":
+            return add(left,right);
+        case "-":
+            return subtract(left,right);
+        case "*":
+            return multiply(left,right);
+        case "/":
+            return divide(left,right);
+    }
+}
+
+const calculation = document.querySelector(".calculation p");
+const buttons = document.querySelectorAll("button");
+const answer = document.querySelector(".answer p");
+
+let previousAns = 0;
+let currentCalc = [];
+let currentNum = [], currentOp = "", nextOp = "";
+let num, op, firstNum = null, secondNum = null;
+
+buttons.forEach(button =>{
+    button.addEventListener("click", ()=>{
+        num = /[0-9]/;
+        op = /[+-/*=]/;
+        if(button.innerText.match(num)){
+            currentCalc.push(button.innerText);
+            currentNum.push(button.innerText);
+        } else if(button.innerText.match(op)){
+            currentCalc.push(` ${button.innerText} `);
+            currentNum.push(button.innerText);
+            if(firstNum == null){
+                firstNum = currentNum.join("");
+                firstNum = firstNum.split(`${button.innerText}`);
+                firstNum = parseInt(firstNum[0]);
+                currentOp = button.innerText;
+                currentNum = [];
+                console.log("firstNum: " + firstNum);
+            } else if(secondNum == null){
+                secondNum = currentNum.join("");
+                secondNum = secondNum.split(`${button.innerText}`);
+                nextOp = button.innerText;
+                secondNum = parseInt(secondNum[0]);
+                currentNum = [];
+                previousAns = operate(currentOp, firstNum, secondNum);
+                answer.innerText = previousAns;
+                currentOp = nextOp;
+                firstNum = previousAns;
+                secondNum = null;
+                // if(currentOp == "="){
+                //     currentCalc = [];
+                //     currentCalc.push(previousAns);
+                //     firstNum = null;
+                // }
+                console.log("secondNum: " + secondNum);
+
+            }
+        }
+        
+        calculation.innerText = currentCalc.join("");
+    });
+});
+
