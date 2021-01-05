a7007f64fe400f2d45d713316651b9ce7675d229
Lettuce05 <61034923+Lettuce05@users.noreply.github.com>
2021-01-05 08:39:36 -0800

    Fixed duplicate op entry

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ buttons.forEach(button =>{
             if(currentCalc.includes(" = ")){
                 currentCalc = [];
                 answer.textContent = 0;
+                previousAns = 0;
             }
             currentCalc.push(button.innerText);
             currentNum.push(button.innerText);
@@ -51,16 +52,10 @@ buttons.forEach(button =>{
             if(currentCalc.includes(" = ")){
                 currentCalc = [];
             }
-            if(currentCalc.length > 0 && !currentCalc[currentCalc.length-1].match(op)){
-                //makes sure there is no duplicate enter of a op
-                currentCalc.push(` ${button.innerText} `);
-                currentNum.push(button.innerText);
-                console.log(currentCalc);
-            } else if(currentCalc.length == 0) {
-                currentCalc.push(` ${button.innerText} `);
-                currentNum.push(button.innerText);
-            } 
-            //TODO: Fix the instance if numbers have already been entered and the operator was chosen
+
+            currentCalc.push(` ${button.innerText} `);
+            currentNum.push(button.innerText);
+
             //check if a num was inputted before operation
             if(currentNum[0].match(op)){
                 currentNum = [String(previousAns), button.innerText];
@@ -68,25 +63,36 @@ buttons.forEach(button =>{
                 if(currentCalc[0].match(op)){
                     currentCalc = [previousAns, ` ${button.innerText} `];
                 }
+                
             } else{
-                amountOfNum++;
-                if(amountOfNum == 1){
-                    firstNum = currentNum.join("");
-                    firstNum = firstNum.split(`${button.innerText}`);
-                    firstNum = firstNum[0];
-                    currentNum = [firstNum, button.innerText];
-                } else if(amountOfNum == 2){
-                    //calculate the first operation
-                    secondNum = currentNum.slice(2, currentNum.length-1);
-                    secondNum = secondNum.join("");
-                    console.log(secondNum);
-                    tempNum = operate(currentNum[1], parseInt(currentNum[0]), parseInt(secondNum));
-                    answer.innerText = tempNum;
-                    //re-adjust the currentNum array
-                    currentNum = [String(tempNum), button.innerText];
-                    //decrement the amount of numbers
-                    amountOfNum--;
+                if(currentCalc[currentCalc.length-2].match(op)){
+                    //makes sure multiple signs arent entered concurrently
+                    currentCalc.splice(currentCalc.length-1, 1);
+                    currentNum.splice(currentNum.length-1, 1);
+                } else {
+                    amountOfNum++;
+                    if(amountOfNum == 1){
+                        firstNum = currentNum.join("");
+                        firstNum = firstNum.split(`${button.innerText}`);
+                        firstNum = firstNum[0];
+                        console.log(firstNum);
+                        currentNum = [firstNum, button.innerText];
+                    } else if(amountOfNum == 2){
+                        //calculate the first operation
+                        console.log("made it here");
+                        secondNum = currentNum.slice(2, currentNum.length-1);
+                        secondNum = secondNum.join("");
+                        console.log(currentNum);
+                        console.log(secondNum);
+                        tempNum = operate(currentNum[1], parseInt(currentNum[0]), parseInt(secondNum));
+                        answer.innerText = tempNum;
+                        //re-adjust the currentNum array
+                        currentNum = [String(tempNum), button.innerText];
+                        //decrement the amount of numbers
+                        amountOfNum--;
+                    }
                 }
+
             }
             
         } else if(button.innerText == "="){
